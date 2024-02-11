b1e45f7e1935dc52ade070a82d9479cc5048ef6d
yassinmlk <ml.yassink@gmail.com>
2024-02-11 17:05:04 +0100

    modification class selection

--- core/src/ClassSelection/Selection.java
+++ core/src/ClassSelection/Selection.java
@@ -12,7 +12,7 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class Selection extends ApplicationAdapter {
+public class Selection {
     private Texture backgroundImage;
     private Texture spellSquareImage; // potentiellement inutile ( autant faire les carrés directement en amont sur l'image )
     private Texture rectangleImage; // for PV, PA, PM, and spells
@@ -22,21 +22,19 @@ public class Selection extends ApplicationAdapter {
     private Texture classSquare4Image;
     private Texture classSquare5Image;
     private Texture validationImage;
-    private Music menuSound;
-    private OrthographicCamera camera;
-    private SpriteBatch batch;
-    private Rectangle spellSquare; // potentiellement inutile
-    private Rectangle rectangle;
-    private Rectangle classSquare1;
-    private Rectangle classSquare2;
-    private Rectangle classSquare3;
-    private Rectangle classSquare4;
-    private Rectangle classSquare5;
-    private BitmapFont spellTextFont;
-    private String spellText;
+    public Music menuSound;
+    public Rectangle spellSquare; // potentiellement inutile
+    public Rectangle rectangle;
+    public Rectangle classSquare1;
+    public Rectangle classSquare2;
+    public Rectangle classSquare3;
+    public Rectangle classSquare4;
+    public Rectangle classSquare5;
+    public BitmapFont spellTextFont;
+    public Rectangle validation;
+    public String spellText;
 
-    @Override
-    public void create() {
+    public Selection() {
         // load the images
         backgroundImage = new Texture(Gdx.files.internal("droplet.png"));
         spellSquareImage = new Texture(Gdx.files.internal("bucket.png"));
@@ -54,13 +52,6 @@ public class Selection extends ApplicationAdapter {
         // start the playback of the background music immediately
         menuSound.setLooping(true);
         menuSound.play();
-
-
-        // fix the vision to not be impacted by too big pictures
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 1920, 1080);
-
-        batch = new SpriteBatch();
 
         spellText = "Cliquez sur un sort";
         spellTextFont = new BitmapFont();
@@ -101,40 +92,5 @@ public class Selection extends ApplicationAdapter {
         classSquare5.width = 0;
         classSquare5.height = 0;
 
-    }
-
-    public void render() {
-        ScreenUtils.clear(0, 0, 0.2f, 1);
-
-        camera.update();
-
-        batch.setProjectionMatrix(camera.combined);
-        batch.begin();
-        batch.draw(backgroundImage, 0, 0);
-        spellTextFont.draw(batch, spellText, rectangle.x, rectangle.y + 180);
-        batch.end();
-
-        if(Gdx.input.isTouched()) {
-            Vector3 touchPos = new Vector3();
-            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-            camera.unproject(touchPos);
-            if((touchPos.x <= classSquare1.x && touchPos.x >= classSquare1.x - classSquare1.height) && (touchPos.y >= classSquare1.y && touchPos.y >= classSquare1.y - classSquare1.width )) {
-                // En gros faire classText = "Pa : X            PM : X              PV : X" + System.currentTimeMillis(), séparer en 3 éventuellement
-            }
-            if((touchPos.x <= classSquare2.x && touchPos.x >= classSquare2.x - classSquare2.height) && (touchPos.y >= classSquare2.y && touchPos.y >= classSquare2.y - classSquare2.width )) {
-
-            }
-            if((touchPos.x <= classSquare3.x && touchPos.x >= classSquare3.x - classSquare3.height) && (touchPos.y >= classSquare3.y && touchPos.y >= classSquare3.y - classSquare3.width )) {
-
-            }
-            if((touchPos.x <= classSquare4.x && touchPos.x >= classSquare4.x - classSquare4.height) && (touchPos.y >= classSquare4.y && touchPos.y >= classSquare4.y - classSquare4.width )) {
-
-            }
-            if((touchPos.x <= classSquare5.x && touchPos.x >= classSquare5.x - classSquare5.height) && (touchPos.y >= classSquare5.y && touchPos.y >= classSquare5.y - classSquare5.width )) {
-
-            }
-        }
-
-        // ajouter encore des if cette fois ci pour les sorts ( mais faut connaitre leur coord sur le rectangle prc je les ai pas def separement )
     }
 }
