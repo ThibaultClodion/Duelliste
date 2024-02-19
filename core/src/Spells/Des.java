bff4f4b1003f2fd9d6922645e6cb83f4c156c1ba
RaphaelAsseraf <158580562+RaphaelAsseraf@users.noreply.github.com>
2024-02-19 19:08:54 +0100

    Implémentation de l'attaque Dés

---
+++ core/src/Spells/Des.java
@@ -0,0 +1,17 @@
+package Spells;
+
+/* Inflicts random damage between 50 and 300
+* 4 PA
+* 0 reload
+* 3 range
+ */
+
+public class Des extends Attack {
+    public Des() {
+        super(4, (int) Math.random() * 6 + 1, 3);
+    }
+    @Override
+    public void Launch() {
+        System.out.println("Lance les Dés");
+    }
+}
