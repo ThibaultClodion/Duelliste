3006d28a26418456edb932bb577bd364606f24e2
KelvinMartinez13 <149091978+KelvinMartinez13@users.noreply.github.com>
2024-03-21 07:05:41 +0100

    Ajout Deplacement et DoubleDague

---
+++ core/src/Spells/Deplacement.java
@@ -0,0 +1,10 @@
+package Spells;
+
+import Game.PlayerController;
+
+public class Deplacement implements SpellComponent{
+    public void execute(int[] mapPos, PlayerController otherPlayer, PlayerController myPlayer)
+    {
+
+    }
+}
---
+++ core/src/Spells/DoubleDague.java
@@ -0,0 +1,5 @@
+package Spells;
+
+public class DoubleDague extends Spell{
+    public DoubleDague(){super(new SpellComponent[] {new Damage(300)}, 1, 3);}
+}
