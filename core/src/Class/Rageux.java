d51ff72c5ae910251cb5f02521d52fc7d0800296
RaphaelAsseraf <158580562+RaphaelAsseraf@users.noreply.github.com>
2024-04-07 15:35:37 +0200

    Ajout de la classe rageux

---
+++ core/src/Class/Rageux.java
@@ -0,0 +1,8 @@
+package Class;
+import Spells.*;
+import com.badlogic.gdx.graphics.Texture;
+public class Rageux extends Character {
+    public Rageux () {
+        super (1400f, 4, 5, new Texture("rageux.png"), new Spell[]{});
+    }
+}
