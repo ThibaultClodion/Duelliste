a48bc58acc7c625865c2fee122b3e7a099a087bb
yassinmlk <ml.yassink@gmail.com>
2024-04-26 11:18:46 +0200

    images for Lamenpeine Spell

--- core/src/Spells/Dechiquetage.java
+++ core/src/Spells/Dechiquetage.java
@@ -5,6 +5,6 @@ import com.badlogic.gdx.graphics.Texture;
 public class Dechiquetage extends Spell{
     public Dechiquetage()
     {
-            super(new SpellComponent[] {new Damage(400)}, 4, 1,0, new Texture("classSquare1.JPG"));
+            super(new SpellComponent[] {new Damage(400)}, 4, 1,0, new Texture("Dechiquetage.png"));
     }
 }
--- core/src/Spells/Lame_Sacrificielle.java
+++ core/src/Spells/Lame_Sacrificielle.java
@@ -3,5 +3,5 @@ package Spells;
 import com.badlogic.gdx.graphics.Texture;
 
 public class Lame_Sacrificielle extends Spell{
-    public Lame_Sacrificielle(){super(new SpellComponent[] {new AjoutPmPa()},3,0,0,new Texture("classSquare1.JPG"));}
+    public Lame_Sacrificielle(){super(new SpellComponent[] {new AjoutPmPa()},3,0,0,new Texture("lameSacrificielle.png"));}
 }
