01b7ca2ff942f7d40c1a985d18f0a12c9006fd39
Thibault Clodion <thibaultclodion@gmail.com>
2024-05-03 14:23:47 +0200

    Ajout des spells pour Aleator

--- core/src/Class/Aleator.java
+++ core/src/Class/Aleator.java
@@ -7,7 +7,7 @@ public class Aleator extends Character
 {
     public Aleator()
     {
-        super(1500f, 3, 6, new Texture("aleator.png"), new Texture("aleatorRectangle.JPG"), new Spell[] {new Morsure(),new VolDesorganise(),new Des(),new Carte(),new SoinDouteux(),new RouletteRusse(),new NouvelleChance()});
+        super(1500f, 3, 6, new Texture("aleator.png"), new Texture("aleatorRectangle.JPG"), new Spell[] {new VolDesorganise(),new Des(),new Carte(),new SoinDouteux(),new RouletteRusse(),new NouvelleChance()});
 
     }
 
--- core/src/Spells/SoinDouteux.java
+++ core/src/Spells/SoinDouteux.java
@@ -3,6 +3,6 @@ package Spells;
 import com.badlogic.gdx.graphics.Texture;
 
 public class SoinDouteux extends Spell{
-    public SoinDouteux() {super(new SpellComponent[] {new Heal((int) Math.round(Math.random())*300)},3,0,3, new Texture("classSquare1.JPG"));}
+    public SoinDouteux() {super(new SpellComponent[] {new Heal((int) Math.round(Math.random())*300)},3,0,3, new Texture("SoinDouteux.png"));}
     }
 
