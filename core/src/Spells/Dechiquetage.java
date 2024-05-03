70067ad0637e0685590575fc16a12d502b64c65e
NathanVanier <nathan.vanier@gmail.com>
2024-05-03 13:40:26 +0200

    correction timer

--- core/src/Spells/Dechiquetage.java
+++ core/src/Spells/Dechiquetage.java
@@ -5,6 +5,6 @@ import com.badlogic.gdx.graphics.Texture;
 public class Dechiquetage extends Spell{
     public Dechiquetage()
     {
-            super(new SpellComponent[] {new Damage(400)}, 4, 1,4, new Texture("Dechiquetage.png"));
+            super(new SpellComponent[] {new Damage(400)}, 4, 1,4, new Texture("dechiquetage.png"));
     }
 }
