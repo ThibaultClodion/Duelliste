bf61b6784a47b1e2af0bf34f1dcf65b7b3559db1
KelvinMartinez13 <149091978+KelvinMartinez13@users.noreply.github.com>
2024-05-15 15:00:02 +0200

    Changeemtn de PA Yassin

--- core/src/Class/Lamenpeine.java
+++ core/src/Class/Lamenpeine.java
@@ -8,6 +8,6 @@ public class Lamenpeine extends Character
 
     public Lamenpeine()
     {
-        super(1400f, 2, 5, new Texture("lamenpeine62x62.png"), new Texture("lamenpeine.JPG"), new Spell[] {new SautDeLange(),new Coup_circulaire(), new Coup_d_epee(), new Dechiquetage(),new Explosion(),new Soif_de_sang()});
+        super(1400f, 2, 6, new Texture("lamenpeine62x62.png"), new Texture("lamenpeine.JPG"), new Spell[] {new SautDeLange(),new Coup_circulaire(), new Coup_d_epee(), new Dechiquetage(),new Explosion(),new Soif_de_sang()});
     }
 }
