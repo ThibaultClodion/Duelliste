c4bbdf78652eed2771836a63e4ffd8e9b890cc71
yassinmlk <ml.yassink@gmail.com>
2024-05-15 16:41:16 +0200

    changing some images

--- core/src/Spells/Explosion.java
+++ core/src/Spells/Explosion.java
@@ -3,5 +3,5 @@ package Spells;
 import com.badlogic.gdx.graphics.Texture;
 
 public class Explosion extends Spell{
-    public Explosion(){super(new SpellComponent[] {new Damage(200)},2,2,2,new Texture("classSquare1.JPG"),"Explosion est une attaque qui inflige 200 dégats");}
+    public Explosion(){super(new SpellComponent[] {new Damage(200)},2,2,2,new Texture("explosion.png"),"Explosion est une attaque qui inflige 200 dégats");}
 }
