3f1204440052dcbc9d693c8c97c26900a7a850cc
RaphaelAsseraf <158580562+RaphaelAsseraf@users.noreply.github.com>
2024-05-16 17:06:45 +0200

    Ajout du spell NoWall

---
+++ core/src/Spells/NoWall.java
@@ -0,0 +1,9 @@
+package Spells;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class NoWall extends Spell {
+    public NoWall() {
+        super(new SpellComponent[] {new Deplacement()}, 3, 3, 2, new Texture(""), "Le NoWall permet de rendre les murs franchissables pour se déplacer de 3 cases");
+    }
+}
